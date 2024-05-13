28b9b980bef1d2d3f5264eff64becd188dda4d16
Junsoo Park <matthiaspark0514@gmail.com>
2024-05-13 07:48:27 -0700

    Feat: Displaying the Sign In and Sign Out Buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,35 +1,51 @@
 import Link from "next/link";
 import {
-    Navbar,
-    NavbarBrand,
-    NavbarContent,
-    NavbarItem,
-    Input,
-    Button,
-    Avatar
-} from '@nextui-org/react';
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Input,
+  Button,
+  Avatar,
+} from "@nextui-org/react";
 import { auth } from "@/auth";
 
 export default async function Header() {
-    const session = await auth();
+  const session = await auth();
 
-    return (
-        <Navbar className="shadow mb-6">
-            <NavbarBrand>
-                <Link href='/' className="font-bold">Discuss</Link>
-            </NavbarBrand>
-            <NavbarContent justify="center">
-                <NavbarItem>
-                    <Input />
-                </NavbarItem>
-            </NavbarContent>
-            <NavbarContent justify="end">
-                <NavbarItem>
-                    {
-                        session?.user ? <div>Signed In</div> : <div>Signed Out</div>
-                    }
-                </NavbarItem>
-            </NavbarContent>
-        </Navbar>
+  let authContent: React.ReactNode;
+  if (session?.user) {
+    authContent = <Avatar src={session.user.image || ""} />;
+  } else {
+    authContent = (
+      <>
+        <NavbarItem>
+          <Button type="submit" color="secondary" variant="bordered">
+            Sign in
+          </Button>
+        </NavbarItem>
+        <NavbarItem>
+          <Button type="submit" color="primary" variant="flat">
+            Sign in
+          </Button>
+        </NavbarItem>
+      </>
     );
+  }
+
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Discuss
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Input />
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">{authContent}</NavbarContent>
+    </Navbar>
+  );
 }

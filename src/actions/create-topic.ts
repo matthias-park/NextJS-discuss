89d5825e3af0451522d872c711fc6150a3705f07
Junsoo Park <matthiaspark0514@gmail.com>
2024-05-16 08:07:52 -0700

    Feat: Receiving Form Data

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,10 @@
 "use server";
 
-export async function createTopic() {
+export async function createTopic(formData: FormData) {
+
+  const name = formData.get('name');
+  const description = formData.get('description');
+
+  console.log(name,description)
   //TODO: revlidate the homepage after creating a topic
 }
--- src/components/topics/topic-create-from.tsx
+++ src/components/topics/topic-create-from.tsx
@@ -14,22 +14,26 @@ export default function TopicCreateForm() {
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
-      <PopoverTrigger>
-        <PopoverContent>
-          <form action={actions.createTopic}>
-            <div className="flex flex-col gap-4 p-4 w-80">
-              <h3 className="text-lg">Create a Topic</h3>
-              <Input label="Name" labelPlacement="outside" placeholder="Name" />
-              <Textarea
-                label="Description"
-                labelPlacement="outside"
-                placeholder="Describe your topic"
-              />
-              <Button type="submit">Submit</Button>
-            </div>
-          </form>
-        </PopoverContent>
-      </PopoverTrigger>
+      <PopoverContent>
+        <form action={actions.createTopic}>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a Topic</h3>
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+            />
+            <Textarea
+              name="description"
+              label="Description"
+              labelPlacement="outside"
+              placeholder="Describe your topic"
+            />
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </PopoverContent>
     </Popover>
   );
 }

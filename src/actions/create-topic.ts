0b6b502befacfd5960ec23f6739f2a155426f34b
Junsoo Park <matthiaspark0514@gmail.com>
2024-05-17 07:56:50 -0700

    Feat: UseFormState Type

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,21 +1,40 @@
 "use server";
 
-import {z} from 'zod';
+import { z } from "zod";
 
 const createTopicSchema = z.object({
-  name: z.string().min(3).regex(/[^a-z-]+$/, {message: 'Must be lowercase letters or dashes without spaces'}),
+  name: z
+    .string()
+    .min(3)
+    .regex(/[^a-z-]+$/, {
+      message: "Must be lowercase letters or dashes without spaces",
+    }),
   description: z.string().min(10),
-})
-export async function createTopic(formData: FormData) {
+});
 
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+  };
+}
+export async function createTopic(
+  fromState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
   const result = createTopicSchema.safeParse({
-    name: formData.get('name'),
-    description: formData.get('description')
+    name: formData.get("name"),
+    description: formData.get("description"),
   });
 
-  if(!result.success) {
-
-    console.log(result.error.flatten().fieldErrors);
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    }
   }
+
+  return {
+    errors:{}
+  };
   //TODO: revlidate the homepage after creating a topic
 }

8a30e7d2355319ca79e6b64c5052e94a3f21dc29
Junsoo Park <matthiaspark0514@gmail.com>
2024-05-21 08:06:48 -0700

    Feat: Fetching and Listing Content From Prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import TopicCreateForm from "@/app/topics/topic-create-from";
+import { Divider } from "@nextui-org/react";
+import TopicCreateForm from "@/components/topics/topic-create-from";
+import TopicList from '@/components/topics/topic-list'
 
 export default async function Home() {
   return <div className="grid grid-cols-4 gap-4 p-4">
     <div className="col-span-3">
       <h1 className="text-xl m-2">Top Posts</h1>
     </div>
-    <div>
+    <div className="border shadow py-3 px-2">
       <TopicCreateForm />
+      <Divider className="my-2"/>
+      <h3 className="text-lg"> Topics</h3>
+      <TopicList/>
     </div>
   </div>;
 }

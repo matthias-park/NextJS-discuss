e9eb57a7643028b971b1540479f728ed647f9d88
Junsoo Park <matthiaspark0514@gmail.com>
2024-05-22 09:09:02 -0700

    Feat: Applying Validation to Post Creation

--- src/components/posts/post-create-from.tsx
+++ src/components/posts/post-create-from.tsx
@@ -9,41 +9,45 @@ import {
   PopoverTrigger,
   PopoverContent,
 } from "@nextui-org/react";
-import * as actions from '@/actions';
+import * as actions from "@/actions";
 import FormButton from "../common/form-button";
 
 export default function PostCreateForm() {
+  const [formState, action] = useFormState(actions.createPost, {
+    errors: {},
+  });
+
   return (
     <Popover placement="left">
-        <PopoverTrigger>
-            <Button color="primary">
-                Create a Post
-            </Button>
-        </PopoverTrigger>
-        <PopoverContent>
-            <form>
-                <div className="flex flex-col gap-4 p-4 w-80">
-                    <h3 className="text-lg">Create a Post</h3>
+      <PopoverTrigger>
+        <Button color="primary">Create a Post</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action={action}>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a Post</h3>
 
-                    <Input 
-                        name="title"
-                        label="Title"
-                        labelPlacement="outside"
-                        placeholder="Title"
-                    />
-                    <Input
-                        name="content"
-                        label="Content"
-                        labelPlacement="outside"
-                        placeholder="Content"
-                    />
+            <Input
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join("")}
+              name="title"
+              label="Title"
+              labelPlacement="outside"
+              placeholder="Title"
+            />
+            <Textarea
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join("")}
+              name="content"
+              label="Content"
+              labelPlacement="outside"
+              placeholder="Content"
+            />
 
-                    <FormButton>
-                        Create Post
-                    </FormButton>
-                </div>
-            </form>
-        </PopoverContent>
+            <FormButton>Create Post</FormButton>
+          </div>
+        </form>
+      </PopoverContent>
     </Popover>
   );
 }

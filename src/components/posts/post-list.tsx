b286dd6a398bb3421acf05fe7eb6327e1614e2d1
Junsoo Park <matthiaspark0514@gmail.com>
2024-05-23 08:52:44 -0700

    Feat: Define in Parent, Fetch in Child

--- src/components/posts/post-list.tsx
+++ src/components/posts/post-list.tsx
@@ -1,9 +1,15 @@
-import type { Post, User, Topic } from '@prisma/client';
+import type { PostWithData} from '@/db/queries/posts'
 import Link from 'next/link';
 import paths from "@/paths";
 
+interface PostListProps {
+  fetchData: () => Promise<PostWithData[]>
+}
+
 // TODO: Get list of posts into this component somehow
-export default function PostList() {
+export default async function PostList({fetchData}: PostListProps) {
+  const posts = await fetchData();
+
   const renderedPosts = posts.map((post) => {
     const topicSlug = post.topic.slug;
 

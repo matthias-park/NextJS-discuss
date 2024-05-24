3b95fbc51cead839acbbf452d220880772cb70d9
Junsoo Park <matthiaspark0514@gmail.com>
2024-05-24 09:35:18 -0700

    Feat: Fetching the Big List

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,7 +1,9 @@
 import CommentCreateForm from "@/components/comments/comment-create-form";
+import CommentList from "@/components/comments/comment-list";
 import PostShow from "@/components/posts/post-show";
 import paths from "@/paths";
 import Link from "next/link";
+import { fetchCommentsByPostId } from "@/db/queries/comments";
 
 interface PostShowPageProps {
   params: {
@@ -19,6 +21,7 @@ export default async function PostShowPage({params}: PostShowPageProps) {
       </Link>
       <PostShow postId={postId}/>
       <CommentCreateForm postId={postId} startOpen />
+      <CommentList fetchData={() => fetchCommentsByPostId(postId)}/>
     </div>
   )
 }

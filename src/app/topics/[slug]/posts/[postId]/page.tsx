66f647a9d31f0c2e977bd475ffa812939b902a71
Junsoo Park <matthiaspark0514@gmail.com>
2024-05-24 09:11:27 -0700

    Feat: Don't Go Crazy with Reuse

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,3 +1,22 @@
-export default function PostShowPage() {
-  return <div>Post Show Page</div>;
+import PostShow from "@/components/posts/post-show";
+import paths from "@/paths";
+import Link from "next/link";
+
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  }
+}
+
+export default async function PostShowPage({params}: PostShowPageProps) {
+  const {slug, postId} = params
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-slid" href={paths.topicShow(slug)}>
+        {'<'}Back to {slug}
+      </Link>
+      <PostShow postId={postId}/>
+    </div>
+  )
 }
